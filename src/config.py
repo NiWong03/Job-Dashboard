b028ec0befcc9fc2b3cc7bf6a0bef6bec205bd35
Nicholas Wong <Nickstorm07@gmail.com>
2025-08-01 16:48:08 -0700

    using render postgres

--- src/config.py
+++ src/config.py
@@ -2,14 +2,15 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USERNAME = os.getenv("DB_USERNAME")     
+DB_PASSWORD = os.getenv("DB_PASSWORD")  # Replace with the password you set for jobuser
+DB_HOST = os.getenv("DB_HOST", "localhost")         # Usually localhost for local development            # Default PostgreSQL port
 DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
+
+
 
 # Construct the database URL
-DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-# DATABASE_URL = os.getenv('DATABASE_URL', f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}") 
+DATABASE_URL = "postgresql://jobsdb_woqz_user:lIFp4VmsFEDdf5hEOoVVqVa5WupoGVSV@dpg-d1vvcgfdiees73c2t5m0-a.oregon-postgres.render.com/jobsdb_woqz"
